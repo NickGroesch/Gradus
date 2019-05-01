87ab36b82a40fa4aebf36182cec082b51d4cfa0a
mc0918 <mcampbell0918@gmail.com>
2019-05-01 11:29:42 -0500

    merged different contributors' src folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Home from "./pages/Home/index";
-// import Exercise from "./pages/Exercise/index";
-// import Navbar from "./components/Navbar/Navbar";
-// import LogPage from "./pages/LogPage/index";
-//import "./index.css";
+import Home from "./pages/Home/index";
+import Exercise from "./pages/Exercise/index";
+import Navbar from "./components/Navbar/Navbar";
+import LogPage from "./pages/LogPage/index";
+import "./index.css";
 
 function App() {
   return (
     <Router>
       <div>
-        {/* <Navbar />
+        <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/logPage" component={LogPage} />
           <Route exact path="/exercise" component={Exercise} />
-        </Switch> */}
+        </Switch>
       </div>
     </Router>
   );
---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+import Midi from "../../components/Midi/MidiTest";
+
+function Home() {
+  return (
+    <div>
+      <div className="welcome">Welcome to GRADUS</div>
+      <div className="how-to">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <Midi />
+      </div>
+    </div>
+  );
+}
+export default Home;
