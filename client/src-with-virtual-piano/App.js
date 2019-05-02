a5e25abf0a68adc39d12c533e521900c37ed08fe
Sarah Tam <sarahtam718@gmail.com>
2019-05-02 10:51:59 -0500

    resolved conflicts

--- client/src-with-virtual-piano/App.js
+++ client/src-with-virtual-piano/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// Nick Component
-import Graph from "./components/graphs"
+
 // Sarah Component
-// import Piano from "./components/virtualPiano/virtualPiano";
+import Piano from "./components/virtualPiano/virtualPiano";
 
 //Mahfouz components
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar";
 import Register from "./components/Register";
-import Home from "./components/Home";
+// import Home from "./components/Home";
 import Login from "./components/Login";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -18,14 +17,12 @@ import { setCurrentUser, logoutUser } from "./actions/authentication";
 
 //Michael components
 import Midi from "./components/Midi/MidiTest";
-// import Abcjs from "react-abcjs";
 
 //Ky components
 import LogPage from "./pages/LogPage/index";
-import Landing from "./pages/Home/index";
+import Landing from "./pages/Landing/index";
 import Exercise from "./pages/Exercise/index";
 import "./index.css";
-import Graphs from "./components/graphs";
 
 if (localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken);
@@ -46,26 +43,16 @@ function App() {
         <div>
           <Navbar />
           <Route exact path="/" component={Landing} />
-          <Route exact path="/exercise" component={Exercise} />
-          <Graphs />
-          {/* <Midi /> */}
-          {/* <Abcjs */}
-          {/* abcNotation={ */}
-          {/* //X: 1 stave T: title of rendered staff C: composer K: key(G in this case) "|": bar line */}
-          {/* "X:1\nT:Example\nM:4/4\nC:Trad.\nK:G\n|:gc'c,c dedB|dedB dedB|c2ec B2dB|c2A2 A2BA|" */}
-          {/* } */}
-          {/* parserParams={{}} */}
-          {/* engraverParams={{ responsive: "resize" }} */}
-          {/* renderParams={{ viewportHorizontal: true }} */}
-          {/* /> */}
-          {/* </Route>*/}
+          <Route exact path="/exercise" component={Exercise}>
+            <Midi />
+          </Route>
           <div className="container">
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
           </div>
         </div>
       </Router>
-    </Provider >
+    </Provider>
     //Mahfouz app
 
     //===================================
---
+++ client/src-with-virtual-piano/components/virtualPiano/virtualPiano.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import "./virtualPiano.css";
+
+let keyData;
+class Piano extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            keysPlayed: [],
+            octaveCount: 0
+        }
+        // this.octaveIncrement = this.octaveIncrement.bind(this);
+        // console.log(this)
+    }
+
+
+    octaveIncrement = () => {
+        this.setState(({ octaveCount: this.state.octaveCount + 12 }));
+        console.log("add 12")
+    }
+
+    octaveDecrement = () => {
+        this.setState(({ octaveCount: this.state.octaveCount - 12 }));
+        console.log("subtract 12")
+    }
+
+    pianoKeyClick = () => {
+        console.log("piano key clicked");
+    }
+    // $(".pianoKey").click(function () {
+    //     // var keyData = $(this)[0].id;
+    //     keyData = $(this)[0].id;
+    //     // keysPlayed.push(keyData);
+    //     // console.log(keysPlayed)
+    //     console.log(keyData);
+    //     return keyData;
+    //     // var audioClip = $("#c_octave1_audio")[0];
+    //     // console.log("the sound: ", audioClip);
+    //     // audioClip.play();
+    // });
+
+    render() {
+        return (
+            <div className="virtual-piano">
+                <div className="svg-container">
+                    <svg className="piano">
+                        <polygon points="200,10 230,10 230,100 245,100 245,220 200,220 200,10" className="white pianoKey"
+                            id="0" onClick={this.pianoKeyClick} />
+                        <polygon points="245,100 260,100 260,10 275,10 275,100 290,100 290,220 245,220 245,100"
+                            className="white pianoKey" id="2" />
+                        <polygon points="305,10 335,10 335,220 290,220 290,100 305,100 305,10" className="white pianoKey" id="4" />
+                        <polygon points="335,10 365,10 365,100 380,100 380,220 335,220 335,10" className="white pianoKey" id="5" />
+                        <polygon points="380,100 395,100 395,10 410,10 410,100 425,100 425,220 380,220 380,100"
+                            className="white pianoKey" id="7" />
+                        <polygon points="425,100 440,100 440,10 455,10 455,100 470,100 470,220 425,220 425,100"
+                            className="white pianoKey" id="9" />
+                        <polygon points="470,100 485,100 485,10 515,10 515,220 470,220 470,100" className="white pianoKey" id="11" />
+
+                        <polygon points="230,10 260,10 261,130 231,130 230,10" className="black pianoKey" id="1" />
+                        <polygon points="275,10 305,10 306,130 276,130 275,10" className="black pianoKey" id="3" />
+                        <polygon points="365,10 395,10 396,130 366,130 365,10" className="black pianoKey" id="6" />
+                        <polygon points="410,10 440,10 441,130 411,130 410,10" className="black pianoKey" id="8" />
+                        <polygon points="455,10 485,10 486,130 456,130 455,10" className="black pianoKey" id="10" />
+                    </svg>
+                </div>
+                <button id="octave-up" onClick={this.octaveIncrement}>octave +</button>
+                <button id="octave-down" onClick={this.octaveDecrement}>octave -</button>
+            </div>
+        )
+    }
+}
+
+export default Piano;
+
+// TO DO
+// get piano to show up in react
+// display array chosen to user
+// get state to change on click
+// change css state from active onclick to passive onclick
