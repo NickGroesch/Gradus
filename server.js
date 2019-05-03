3927a80a119e88d040f8f2aa2fef83633433e446
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-05-03 13:53:21 -0500

    added build reference

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ const passport = require('passport');
 const config = require('./db');
 const users = require('./serverRoutes/user');
 //=====DB/login dependencies
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 //React always uses Port 3000
 //var PORT = process.env.PORT || "production"; //for later
