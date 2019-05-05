8fa37dbc02af4e1508f39497406bcfcabc4253e7
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2019-05-05 15:52:30 -0500

    single line range test works

--- utils/tests.js
+++ utils/tests.js
@@ -26,9 +26,9 @@ const keyComb = (testArray, key) => {
 
 // // Cantus Tests
 // Length- cantus create only. submit.
-const lengthCF = (midiArray) => {
+const lengthCF = (dualArray) => {
     let result = "length passes"
-    if (!(midiArray.length < 17 && midiArray.length > 7)) {
+    if (!(dualArray.length < 17 && dualArray.length > 7)) {
         result = "CF Length must be between 8-16 notes"
     }
     return result
@@ -48,16 +48,16 @@ const deltaRange = deltaArray => {
     if (!result.length) { result[0] = "all deltas within octave" }
     return result
 }
-let deltaRangeTestPass = [
-    ["", "", "", [12]],
-    ["", "", "", [0]],
-    ["", "", "", [-12]]
-]
-let deltaRangeTestFail = [
-    ["", "", "", [13]],
-    ["", "", "", [0]],
-    ["", "", "", [-13]]
-]
+// let deltaRangeTestPass = [
+//     ["", "", "", [12]],
+//     ["", "", "", [0]],
+//     ["", "", "", [-12]]
+// ]
+// let deltaRangeTestFail = [
+//     ["", "", "", [13]],
+//     ["", "", "", [0]],
+//     ["", "", "", [-13]]
+// ]
 // console.log(deltaRange(deltaRangeTestPass))//pass
 // console.log(deltaRange(deltaRangeTestFail))//fail [0,2]
 
@@ -74,16 +74,45 @@ const deltaDissonantLeaps = deltaArray => {
     if (!result.length) { result[0] = "pass: no dissonant leaps/ chromatic 1/2steps" }
     return result
 }
-let deltaDissLeapTestPass = [
-    ["", "perf", "octave", [12]],
-    ["", "maj", "third", [-4]],
-    ["", "min", "third", [3]]
-]
-let deltaDissLeapTestFail = [
-    ["", "dim", "octave", [11]],
-    ["", "aug", "unison", [1]],
-    ["", "perf", "unison", [0]],
-    ["", "minor", "seventh", [-10]]
-]
-console.log(deltaDissonantLeaps(deltaDissLeapTestPass))//pass
-console.log(deltaDissonantLeaps(deltaDissLeapTestFail))// fail [0,1,3]
+// let deltaDissLeapTestPass = [
+//     ["", "perf", "octave", [12]],
+//     ["", "maj", "third", [-4]],
+//     ["", "min", "third", [3]]
+// ]
+// let deltaDissLeapTestFail = [
+//     ["", "dim", "octave", [11]],
+//     ["", "aug", "unison", [1]],
+//     ["", "perf", "unison", [0]],
+//     ["", "minor", "seventh", [-10]]
+// ]
+// console.log(deltaDissonantLeaps(deltaDissLeapTestPass))//pass
+// console.log(deltaDissonantLeaps(deltaDissLeapTestFail))// fail [0,1,3]
+const rangeCF = (dualArray) => {
+    let result = ""
+    let low = [127, 0]
+    let high = [0, 0]
+    dualArray.forEach((value, index) => {
+        console.log(value.midi)
+        if (value.midi < low[0]) { low = [value.midi, index] }
+        if (value.midi > high[0]) { high = [value.midi, index] }
+    })
+    let range = high[0] - low[0]
+    if (range > 16) { result = `range exceeds a tenth` } else { result = `range acceptable (${low[0]} at pos. ${low[1]} to ${high[0]} at pos. ${high[1]}) ` }
+    return result
+}
+// let rangeCFtestPass = [
+//     { midi: 60 },
+//     { midi: 58 },
+//     { midi: 68 },
+//     { midi: 74 },
+//     { midi: 72 },
+
+// ]
+// let rangeCFtestFail = [
+//     { midi: 60 },
+//     { midi: 58 },
+//     { midi: 68 },
+//     { midi: 75 }
+// ]
+// console.log(rangeCF(rangeCFtestPass))//pass (high:3, low:(1))
+// console.log(rangeCF(rangeCFtestFail))// fail
