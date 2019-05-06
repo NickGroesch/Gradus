59dd63ceca583117eb86a9f91ee915472725fe5f
mc0918 <mcampbell0918@gmail.com>
2019-05-06 11:40:21 -0500

    un-commented login code for heroku deployent

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,18 @@ import Graph from "./components/graphs";
 // import Piano from "./components/virtualPiano/virtualPiano";
 
 //Mahfouz components
-// import Navbar from "./components/Navbar/Navbar";
-// import Register from "./components/Register";
+import Navbar from "./components/Navbar/Navbar";
+import Register from "./components/Register";
 import Home from "./pages/Home/Home";
-// import Login from "./components/LogIn/Login";
-// import { Provider } from "react-redux";
-// import store from "./components/actions/store";
-// import jwt_decode from "jwt-decode";
-// import setAuthToken from "./components/actions/setAuthToken";
-// import {
-//   setCurrentUser,
-//   logoutUser
-// } from "./components/actions/authentication";
+import Login from "./components/LogIn/Login";
+import { Provider } from "react-redux";
+import store from "./components/actions/store";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./components/actions/setAuthToken";
+import {
+  setCurrentUser,
+  logoutUser
+} from "./components/actions/authentication";
 
 //Michael components
 import Midi from "./components/Midi/MidiTest";
@@ -28,17 +28,17 @@ import Landing from "./pages/Landing/index";
 import "./index.css";
 import Graphs from "./components/graphs";
 
-// if (localStorage.jwtToken) {
-//   setAuthToken(localStorage.jwtToken);
-//   const decoded = jwt_decode(localStorage.jwtToken);
-//   store.dispatch(setCurrentUser(decoded));
+if (localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(decoded));
 
-//   const currentTime = Date.now() / 1000;
-//   if (decoded.exp < currentTime) {
-//     store.dispatch(logoutUser());
-//     window.location.href = "/login";
-//   }
-// }
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser());
+    window.location.href = "/login";
+  }
+}
 
 function App() {
   return (
