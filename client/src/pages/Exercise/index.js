5d27ef0cc4d1cc10f892759112be51a2a87b2ee9
Sarah Tam <sarahtam718@gmail.com>
2019-05-06 14:03:13 -0500

    clear & back buttons working in piano

--- client/src/pages/Exercise/index.js
+++ client/src/pages/Exercise/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Graphs from "../../components/graphs"
 import Midi from "../../components/Midi/MidiTest"
+import Piano from "./../../components/virtualPiano/virtualPiano"
 
 function Exercise() {
   return (
     <div>
+      <a href="/home">Dashboard</a>
       <h1>EXERCISE</h1>
-      <Graphs />
-      <Midi />
+      {/* <Graphs />
+      <Midi /> */}
+      <Piano />
     </div>
   );
 }
