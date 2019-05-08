6518d1659fe0246c6f9561e3cb75558218e0c3df
mc0918 <mcampbell0918@gmail.com>
2019-05-08 09:50:51 -0500

    heroku troubleshooting

--- client/src/utils/API/APIroute1.js
+++ client/src/utils/API/APIroute1.js
@@ -8,21 +8,21 @@ export default {
     console.log("analyze client API", data);
 
     //If on local use localhost... if not use /api/graphs/analyze
-    return axios.post("http://localhost:5000/api/graphs/analyze", data);
-    // return axios.post("/api/graphs/analyze", data);
+    // return axios.post("http://localhost:5000/api/graphs/analyze", data);
+    return axios.post("/api/graphs/analyze", data);
   },
   cantusFirmusSuite: data => {
     console.log("CFsuite clientAPI", data);
-    return axios.post("http://localhost:5000/api/graphs/cantusSuite", data);
-    // return axios.post("api/graphs/cantusSuite", data)
+    // return axios.post("http://localhost:5000/api/graphs/cantusSuite", data);
+    return axios.post("/api/graphs/cantusSuite", data);
   },
   counterpointSuite: data => {
     console.log("CPsuite clientAPI", data);
-    return axios.post(
-      "http://localhost:5000/api/graphs/counterpointSuite",
-      data
-    );
-    // return axios.post("api/graphs/counterpointSuite", data)
+    // return axios.post(
+    //   // "http://localhost:5000/api/graphs/counterpointSuite",
+    //   data
+    // );
+    return axios.post("/api/graphs/counterpointSuite", data);
     // console.log(data);
   },
   findAll: function() {
